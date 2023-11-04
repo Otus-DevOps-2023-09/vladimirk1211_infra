dd9fcee09584b6ee887ca49053d6b960f0870bb9
Vladimir Kudryavtsev <vladimirkudryavc@yandex.ru>
2023-11-04 23:28:54 +0700

    Homework №6
    - Пересобрал образ из прерыдущего ДЗ
    - Установил terraform
    - Добавил файлы в .gitignore
    - Создал сервисный аккаунт
    - Установил плагин на terraform для провайдера YC
    - Сконфигурировал файл main.tf и создал VM
    - Сгенерировал ключи для пользователя ubuntu
    - Пересоздал VM с метаданными пользователя ubuntu
    - Сконфигурировал файл outputs.tf
    - Добавил провиженеры, файл puma.service, файл deploy.sh, раздел connection и пересоздал ресурс VM
    - Создал файл variables.tf, параметризировал main.tf, создал файл terraform.tfvars и перксоздал VM
    - Определил переменную private_key_path, переменную для зоны оставил по умолчанию.
    - Отформатировал все конфигурационные файлы используя команду terraform fmt, создал файл terraform.tfvars.example
    - Пересоздал VM

---
+++ terraform/files/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+APP_DIR=${1:-$HOME}
+
+until sudo apt-get install -y git 2>&1;
+do
+ sleep 1
+done
+
+git clone -b monolith https://github.com/express42/reddit.git $APP_DIR/reddit
+cd $APP_DIR/reddit
+bundle install
+sudo mv /tmp/puma.service /etc/systemd/system/puma.service
+sudo systemctl start puma
+sudo systemctl enable puma
